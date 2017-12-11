8e024be24241366e4cb13d8efe4a8651c1d73028
Burin Choomnuan <agilecreativity@gmail.com>
2017-12-11 10:22:31 -0500

    Use Clojure 1.9.x - yes!
    - Add get-extension

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject cucl "0.1.0"
   :description "Collection of useful Clojure libraries."
-  :url "http://github.com/agilecreativity"
+  :url "http://github.com/agilecreativity/cucl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljfmt "0.5.6"]
             [lein-auto "0.1.3"]]
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-time "0.13.0"]
-                 [me.raynes/fs "1.4.6"]
                  [clj-ssh "0.5.14"]
                  [easy-config "0.1.2"]
-                 ;; [fs "1.3.3"]
                  [me.raynes/fs "1.4.6"]])
