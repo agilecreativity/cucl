28238d7e76b694805ba586a9ad7d3f7af4621190
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-01 18:29:35 -0500

    Update clj-time to latest version

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/agilecreativity/cucl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljfmt "0.5.6"]
+  :plugins [[lein-cljfmt "0.5.7"]
             [lein-auto "0.1.3"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-time "0.13.0"]
+                 [clj-time "0.14.2"]
                  [me.raynes/fs "1.4.6"]
                  [aero "1.1.3"]])
