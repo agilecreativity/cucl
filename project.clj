ff82878d7b0ae2490cad369a5fe7c5c946179013
Burin, Choomnuan <agilecreativity@gmail.com>
2020-10-31 14:17:16 -0400

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject cucl "0.1.12"
+(defproject cucl "0.1.13"
   :description "Collection of useful Clojure libraries."
   :url "http://github.com/agilecreativity/cucl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljfmt "0.6.4"]
-            [lein-auto "0.1.3"]
-            [lein-cloverage "1.0.13"]]
+  :plugins [[lein-cljfmt "0.7.0"]
+            [lein-auto "0.1.4"]
+            [lein-cloverage "1.2.1"]]
   :dependencies [[aero "1.1.6"]
                  [alembic "0.3.2"]
-                 [camel-snake-kebab "0.4.1"]
+                 [camel-snake-kebab "0.4.2"]
                  [circleci/circleci.test "0.4.3"]
                  [clj-time "0.15.2"]
                  [lambdaisland/ansi "0.1.6"]
                  [clj-commons/fs "1.5.2"]
-                 [org.clojure/clojure "1.10.2-alpha1"]]
+                 [org.clojure/clojure "1.10.2-alpha2"]]
   :aliases {"test"  ["run" "-m" "circleci.test/dir" :project/test-paths]
             "tests" ["run" "-m" "circleci.test"]
             "retest" ["run" "-m" "circleci.test.retest"]})
